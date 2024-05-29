00bbc4992a97eb57b63dfb03270f6d243c1029d5
gerson aguiar <gersoncafilho@gmail.com>
2024-05-29 17:34:55 -0300

    Service section

--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,13 @@
 import Nav from './Nav'
 import Button from './Button'
 import ShoeCard from './ShoeCard';
+import ServiceCard from './ServiceCard';
 import PopularProductCard from './PopularProductCard';
 
 export {
     Button,
     Nav,
     PopularProductCard,
+    ServiceCard,
     ShoeCard
 };
