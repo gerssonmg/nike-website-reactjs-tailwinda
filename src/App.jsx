ac569c0673391d5600726e36e72af05ba68cacf2
gerson aguiar <gersoncafilho@gmail.com>
2024-05-29 16:39:54 -0300

    NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ import {
 const App = () => {
   return (
     <main className='relative'>
-      Nav
+      <Nav />
       <section className='xl:padding-l wide:padding-r padding-b'>
         <Hero />
       </section>
